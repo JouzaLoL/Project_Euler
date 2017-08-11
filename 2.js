bfbdc4281c77a4655747e9270f22e6bc38217e01
Josef Vacek <vacekj@outlook.com>
2017-08-11 19:25:55 +0200

    Initial commit

--- 2.js
+++ 2.js
@@ -1,31 +1,27 @@
-function fib(x) //return all fibb numbers smaller or equal to x in an array
-{
-	var arr = [];
-	var a = 1;
+// return all fibb numbers smaller or equal to x in an array 
+function fib(x) {
+  var arr = [];
+  var a = 1;
   var b = 1;
-  while (a <= x)
-    {
-      a += b;
-      b = a - b;
-      arr.push(b);
-    }
+  while (a <= x) {
+    a += b;
+    b = a - b;
+    arr.push(b);
+  }
   return arr;
 }
 
 var arr = fib(4000000); //create an array with all the fibb numbers that are lower than or equal to 4 million
 
-function sum()
-{
+function sum() {
   var s = 0;
-  for (x=0;x<arr.length;x++){
-    if (arr[x] % 2 === 0){ //if a number is even, add it to the sum
-    	    s += arr[x];
+  for (x = 0; x < arr.length; x++) {
+    if (arr[x] % 2 === 0) { //if a number is even, add it to the sum
+      s += arr[x];
 
     }
   }
   return s;
 }
 
-sum();
-
-
+const solution = sum();
---
+++ README.md
@@ -0,0 +1,10 @@
+# Project structure
+``index.js`` is the main file for the current problem. 
+Whenever a problem is solved, it is named as ``#.js``, where # represents the problem number.
+
+### Code style
+I try to follow Google code style with some minor modifications.
+
+### Solution in code
+
+Solution is marked as ``const solution``
